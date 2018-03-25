17b5c7d9e03b49b66a5edcd44db37e96e91a8745
Benni Asal <benedikt.asal@gmx.de>
2018-03-25 21:43:53 +0200

    [TASK] - Render players from db to the view

--- client/main.js
+++ client/main.js
@@ -1,4 +1,37 @@
-import someDefault from "../imports/utils";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
 
-console.log('Log from /client/main.js');
-console.log(someDefault);
+import {Players} from './../imports/api/players';
+
+const renderPlayers = function (playersList) {
+
+    return playersList.map(function (player) {
+        return <p key={player._id}>{player.name} has {player.score} point(s).</p>
+    })
+};
+
+Meteor.startup(function () {
+
+    Tracker.autorun(function () {
+        let players = Players.find().fetch();
+
+        let name = 'Benni';
+        let title = "Meteor App!";
+        let jsx = (
+            <div>
+                <h1>{title}</h1>
+                <p>Hello {name}, It's nice to see you</p>
+                {renderPlayers(players)}
+            </div>
+        );
+        ReactDOM.render(jsx, document.getElementById('app'));
+    });
+
+    Players.insert({
+        name : 'Falk',
+        score: 39
+    });
+
+});
