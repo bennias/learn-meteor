bf84c299bc1b3fa16f578c2c821994af289cb517
Benni Asal <benedikt.asal@gmx.de>
2018-03-31 17:42:30 +0200

    [CONFIG] - Added eslint with prettier

--- client/main.js
+++ client/main.js
@@ -1,53 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Meteor } from "meteor/meteor";
-import { Tracker } from "meteor/tracker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 
-import { Players } from "./../imports/api/players";
+import { Players } from './../imports/api/players';
 
-const renderPlayers = playersList => {
-  return playersList.map(player => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s).
-        <button
-          onClick={() => Players.update(player._id, { $inc: { score: 1 } })}
-        >
-          +
-        </button>
-        <button
-          onClick={() => Players.update(player._id, { $inc: { score: -1 } })}
-        >
-          -
-        </button>
-        <button onClick={() => Players.remove(player._id)}>Remove</button>
-      </p>
-    );
-  });
-};
+const renderPlayers = playersList =>
+  playersList.map(player => (
+    <p key={player._id}>
+      {player.name} has {player.score} point(s).
+      <button onClick={() => Players.update(player._id, { $inc: { score: 1 } })}>+</button>
+      <button onClick={() => Players.update(player._id, { $inc: { score: -1 } })}>-</button>
+      <button onClick={() => Players.remove(player._id)}>Remove</button>
+    </p>
+  ));
 
 const addPlayer = e => {
-  let playerName = e.target.playerName.value;
-  let playerScore = e.target.playerScore.valueAsNumber;
+  const playerName = e.target.playerName.value;
+  const playerScore = e.target.playerScore.valueAsNumber;
   e.preventDefault();
 
   if ((playerName, playerScore)) {
-    e.target.playerName.value = "";
+    e.target.playerName.value = '';
     e.target.playerScore.valueAsNumber = 0;
     Players.insert({
       name: playerName,
-      score: playerScore
+      score: playerScore,
     });
   }
 };
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    const players = Players.find().fetch();
 
-    let name = "Benni";
-    let title = "Meteor App!";
-    let jsx = (
+    const name = 'Benni';
+    const title = 'Meteor App!';
+    const jsx = (
       <div>
         <h1>{title}</h1>
         <p>Hello {name}, Nice to see you</p>
@@ -61,6 +50,6 @@ Meteor.startup(() => {
         </form>
       </div>
     );
-    ReactDOM.render(jsx, document.getElementById("app"));
+    ReactDOM.render(jsx, document.getElementById('app'));
   });
 });
