06a9cd9a7b3f4b759d48c96b30ccace977de63a6
Benni Asal <benedikt.asal@gmx.de>
2018-03-31 12:15:11 +0200

    [REFACTOR] - Added proper formation

--- client/main.js
+++ client/main.js
@@ -1,66 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Meteor} from 'meteor/meteor';
-import {Tracker} from 'meteor/tracker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Meteor } from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
 
-import {Players} from './../imports/api/players';
+import { Players } from "./../imports/api/players";
 
-const renderPlayers = (playersList) => {
-
-    return playersList.map((player) => {
-        return (
-            <p key={player._id}>
-                {player.name} has {player.score} point(s).
-                <button onClick={() => Players.update(player._id, {$inc:{score: 1}})}>
-                    +
-                </button>
-                <button onClick={() => Players.update(player._id, {$inc:{score: -1}})}>
-                    -
-                </button>
-                <button onClick={() => Players.remove(player._id)}>
-                    Remove
-                </button>
-            </p>
-        );
-    });
+const renderPlayers = playersList => {
+  return playersList.map(player => {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s).
+        <button
+          onClick={() => Players.update(player._id, { $inc: { score: 1 } })}
+        >
+          +
+        </button>
+        <button
+          onClick={() => Players.update(player._id, { $inc: { score: -1 } })}
+        >
+          -
+        </button>
+        <button onClick={() => Players.remove(player._id)}>Remove</button>
+      </p>
+    );
+  });
 };
 
-const addPlayer = (e) => {
-    let playerName = e.target.playerName.value;
-    let playerScore = e.target.playerScore.valueAsNumber;
-    e.preventDefault();
+const addPlayer = e => {
+  let playerName = e.target.playerName.value;
+  let playerScore = e.target.playerScore.valueAsNumber;
+  e.preventDefault();
 
-    if (playerName, playerScore){
-        e.target.playerName.value = '';
-        e.target.playerScore.valueAsNumber = 0;
-        Players.insert({
-            name: playerName,
-            score: playerScore
-        });
-    };
+  if ((playerName, playerScore)) {
+    e.target.playerName.value = "";
+    e.target.playerScore.valueAsNumber = 0;
+    Players.insert({
+      name: playerName,
+      score: playerScore
+    });
+  }
 };
 
 Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
 
-    Tracker.autorun(() => {
-        let players = Players.find().fetch();
+    let name = "Benni";
+    let title = "Meteor App!";
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}, Nice to see you</p>
+        <hr />
+        {renderPlayers(players)}
 
-        let name = 'Benni';
-        let title = "Meteor App!";
-        let jsx = (
-            <div>
-                <h1>{title}</h1>
-                <p>Hello {name}, Nice to see you</p>
-                <hr/>
-                {renderPlayers(players)}
-
-                <form onSubmit={addPlayer}>
-                    <input type="text" name="playerName" placeholder="Player Name"/>
-                    <input type="number" name="playerScore" placeholder="Player Score"/>
-                    <button>Add new Player</button>
-                </form>
-            </div>
-        );
-        ReactDOM.render(jsx, document.getElementById('app'));
-    });
+        <form onSubmit={addPlayer}>
+          <input type="text" name="playerName" placeholder="Player Name" />
+          <input type="number" name="playerScore" placeholder="Player Score" />
+          <button>Add new Player</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById("app"));
+  });
 });
