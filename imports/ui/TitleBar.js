f2fdc006df3e558a6acb59a7a742910c71246e1c
Benni Asal <benedikt.asal@gmx.de>
2018-05-01 13:02:30 +0200

    refactor: set subtitle to optional

--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -2,18 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class TitleBar extends React.Component {
+  renderSubtitle() {
+    if (this.props.subtitle) {
+      return <h2>{this.props.subtitle}</h2>;
+    }
+  }
   render() {
     return (
       <div>
         <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
+        {this.renderSubtitle()}
       </div>
     );
   }
 }
 TitleBar.propTypes = {
   title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
 };
 
 export default TitleBar;
