58efae645226e96ce00d68d59341161cb7a7cea5
Benni Asal <benedikt.asal@gmx.de>
2018-05-07 22:49:40 +0200

    task: add react-flip-move lib

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,16 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import FlipMove from 'react-flip-move';
 import Player from './../ui/Player';
 
 class PlayerList extends React.Component {
   renderPlayers() {
     if (this.props.players.length === 0) {
-      return <p>Add a new Player</p>;
+      return (
+        <div className="item">
+          <p className="item__message item__message--light">Add a new Player</p>
+        </div>
+      );
     }
     return this.props.players.map(player => <Player key={player._id} player={player} />);
   }
   render() {
-    return <div className="playerList">{this.renderPlayers()}</div>;
+    return (
+      <div className="playerList">
+        <FlipMove maintainContainerHeight={true} duration={300} easing="ease-out">
+          {this.renderPlayers()}
+        </FlipMove>
+      </div>);
   }
 }
 
--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -4,13 +4,13 @@ import React from 'react';
 class TitleBar extends React.Component {
   renderSubtitle() {
     if (this.props.subtitle) {
-      return <h2>{this.props.subtitle}</h2>;
+      return <h2 className="titleBar__subtitle">{this.props.subtitle}</h2>;
     }
   }
   render() {
     return (
       <div className="titleBar">
-        <h1>{this.props.title}</h1>
+        <h1 className="titleBar__title">{this.props.title}</h1>
         {this.renderSubtitle()}
       </div>
     );
