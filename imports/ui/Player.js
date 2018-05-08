d02f490faf17930178e6bd145a4a74f07c911bdb
Benni Asal <benedikt.asal@gmx.de>
2018-05-08 20:18:02 +0200

    task: add player-ranking

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -4,11 +4,14 @@ import { Players } from './../api/players';
 
 class Player extends React.Component {
   render() {
+    const dynamicClassName = `item item--position-${this.props.player.rank}`;
+
     return (
-      <div className="item">
+      <div className={dynamicClassName}>
         <p className="item__description" key={this.props.player._id}>
           {this.props.player.name} has {this.props.player.score} point(s).
         </p>
+        <p className="item__description">{this.props.player.position} place</p>
         <button
           className="button button--rounded"
           onClick={() => Players.update(this.props.player._id, { $inc: { score: 1 } })}
